10859c58de9d603defa2a7a3ab9133ba8acd2843
huzefasabir <146101520+huzefasabir@users.noreply.github.com>
2025-07-21 23:59:54 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+
 import 'package:currency/material_page.dart';
+import 'package:flutter/material.dart';
 void main(){
   runApp(const MyApp());
 }
--- lib/material_page.dart
+++ lib/material_page.dart
@@ -5,16 +5,53 @@ class MaterialPages extends StatelessWidget{
 
   @override
   Widget build (BuildContext context){
-    return const Scaffold(
+    final border= OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                    strokeAlign: BorderSide.strokeAlignInside,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                );
+    return Scaffold(
+      //backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("0"),
+            const Text('0',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: Colors.white
+              ),
+            ),
+            TextField(
+              style: const TextStyle(
+                color: Color.fromARGB(255, 27, 43, 225),
+              ),
+              decoration: InputDecoration(
+                // label: Text("Please Enter the Amount"),
+                // labelStyle: TextStyle(
+                //   color: Colors.white,
+                //   fontSize:30,
+                // ),
+                //helperText: 'Please enter the amount in USD'
+                hintText: 'Please enter the amount in USD',
+                hintStyle: const TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: const Icon(Icons.monetization_on, color: Colors.black,),//size: 25,),
+                fillColor: Colors.white,
+                filled: true,
+                focusedBorder:border,
+                enabledBorder:border,
+              ),
+            ),
           ],
         ),
       ),
     );
-
   }
 }
